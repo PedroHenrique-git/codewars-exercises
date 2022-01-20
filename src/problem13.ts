55dfef238571d70ce8ce35adb97e2e0c564436b6
PedroHenrique-git <pedromf34@outlook.com>
2022-01-19 22:15:04 -0300

    problem 13 finish

--- src/problem13.ts
+++ src/problem13.ts
@@ -1,11 +1,14 @@
 export class G964 {
+    private static alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+
     public static playPass(s: string, n: number): string {
         const arrS = s.split('');
 
         const newLetters = arrS.map((l, index) => {
             if( l.match(/[a-z]/i) ) {
-                const code = l.charCodeAt(0) + n;
-                return index % 2 === 0 ? String.fromCharCode(code).toLocaleUpperCase() : String.fromCharCode(code).toLocaleLowerCase();
+                const letterPosition = this.alphabet.indexOf(l.toLowerCase());
+                const newLetter = (letterPosition + n) % 26;
+                return index % 2 === 0 ? this.alphabet[newLetter].toLocaleUpperCase() : this.alphabet[newLetter].toLowerCase();
             }
 
             if( l.match(/[0-9]/) ) {
@@ -23,4 +26,4 @@ export class G964 {
 
 const val = G964.playPass("I LOVE YOU!!!", 1);
 
-console.log(val === "!!!vPz fWpM J");
+console.log(val);
